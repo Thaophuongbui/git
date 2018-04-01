84db814acb0f44a5f0fd0717066b14aefd70a0a0
Thuannt112 <37172442+Thuannt112@users.noreply.github.com>
2018-04-01 21:24:18 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+	$title="Home";
+	$content='  <img src="images/6.jpeg" class="img_1"/>
+                    <center><h3>WELCOME TO OUR COFFEE HOUSE</h3></center>
+                    <center>
+                        <p>From a cultural standpoint, coffeehouses largely serve as centers 
+                        of social interaction: the coffeehouse provides patrons with a place
+                        to congregate, talk, read, write, entertain one another, or pass the time,
+                        whether individually or in small groups.
+                        </p>
+                    </center>
+                    
+                    
+                    <img src="images/hat-cafe.png" class="img_2"/>
+                    <center><h3>THE EXPERIENCE</h3></center>
+                    <center>
+                        <p>The coffee is brewed by first roasting the green coffee beans over 
+                        hot coals in a brazier. Once the beans are roasted each 
+                        participant is given an opportunity to sample the aromatic 
+                        smoke by wafting it towards them. This is followed by 
+                        the grinding.
+                        </p>
+                    </center>';
+        require ('template.php');
+ ?>
+
+
