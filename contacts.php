3e28abf82959879cdb52a1fc332d9a8a7f8251e9
Thuannt112 <37172442+Thuannt112@users.noreply.github.com>
2018-04-01 21:25:20 +0700

    Add files via upload

---
+++ contacts.php
@@ -0,0 +1,18 @@
+<?php 
+	$title="Home";
+	$content='  
+                    <img src="images/contact.PNG" class="img_3"/>
+                    <br>
+                    <center><h3>GET IN TOUCH</h3></center>
+                    <center>
+                        <p>Every Monday, Tuesday and Wednesday evening, we’re offering groups of 
+                        10 or more that book an area in our bar a complimentary bottle of Champagne.
+                        <br>
+                        Open hours: 8.00 - 22.00 Mon-Sat
+                        <br>
+                        Call our reservations team on 0966 173 777 for more information.
+                        </p>
+                    </center>';
+        require ('template.php');
+ ?>
+
