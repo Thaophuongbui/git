d23d32dc2424f78d5db5b775a6d3f4a877169430
1551060631MinhNguyet <37079128+1551060631MinhNguyet@users.noreply.github.com>
2018-04-01 21:00:35 +0700

    Add files via upload

---
+++ management.php
@@ -0,0 +1,15 @@
+<?php
+$title ="management";
+
+$content = '<h3>coffee</h3>
+            <a href="coffeeadd.php">Add new coffee</a>
+            <br/>
+            <a href="UploadImage.php">Upload image</a>
+            <br/>
+            
+            <a href="coffeeOverview.php">Coffee Overview</a><br/>';
+
+include './template.php';
+  
+
+?>          
